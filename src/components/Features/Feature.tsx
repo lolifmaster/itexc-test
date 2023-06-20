fe234c9333844f14520c625669757647cdcc922b
Mounir <polomnr1@gmail.com>
2023-06-21 00:53:39 +0100

    changing icons and hover effect of feutures

--- src/components/Features/Feature.tsx
+++ src/components/Features/Feature.tsx
@@ -11,20 +11,23 @@ const Feature: FC<FeatureProps> = ({ Icon, content, primary = false }) => {
   let newText = content.split("\n");
   return (
     <div
-      className={cn(
-        "flex h-full w-full flex-1 flex-col items-center justify-center p-10 py-5",
-        {
-          "box-content border-l-4 border-orange bg-lightBlue p-0 md:border-b-4 md:border-l-0":
-            primary,
-        }
-      )}
+      // className={cn(
+      //   "flex h-full gap-2 w-full flex-1 flex-col items-center justify-center p-10 py-5",
+      //   {
+      //     "box-content border-l-4 border-orange bg-lightBlue p-0 md:border-b-4 md:border-l-0":
+      //       primary,
+      //   }
+      // )}
+      className="flex h-full w-full flex-1 flex-col items-center justify-center gap-2 px-10 py-5 transition hover:scale-105 hover:border-l-4 hover:border-orange hover:bg-lightBlue hover:md:border-b-4 hover:md:border-l-0 hover:md:p-0"
     >
       <Icon className="h-12 w-12 text-white" />
-      {newText.map((text, index) => (
-        <p className="text-center text-white" key={index}>
-          {text}
-        </p>
-      ))}
+      <div>
+        {newText.map((text, index) => (
+          <p className="text-center text-white" key={index}>
+            {text}
+          </p>
+        ))}
+      </div>
     </div>
   );
 };
--- src/components/Features/index.tsx
+++ src/components/Features/index.tsx
@@ -4,18 +4,18 @@ import { Icons } from "../Icons";
 const Features = () => {
   return (
     <div className="mx-auto grid w-2/3 items-center bg-[#005EA3] md:absolute md:bottom-0 md:translate-x-1/4 md:translate-y-2/3 md:grid-cols-4">
-      <Feature Icon={Icons.mail} content={"Online \n Consultation"} />
+      <Feature Icon={Icons.consult} content={"Online \n Consultation"} />
       <Feature
-        Icon={Icons.mail}
+        Icon={Icons.medical}
         content={"Medical advice \n and second opinions"}
       />
       <Feature
-        Icon={Icons.mail}
+        Icon={Icons.time}
         content={"availability \n  24/7"}
         primary={true}
       />
       <Feature
-        Icon={Icons.mail}
+        Icon={Icons.support}
         content={"Health education \n and resources"}
       />
     </div>
