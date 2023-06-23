d650027b1ef9d02a1bfc8534e524bb36cfd39fdd
Mounir <polomnr1@gmail.com>
2023-06-23 02:20:12 +0100

    remove hero bg from phone screens

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,7 +1,7 @@
 import BackgroundImage from "../assets/bg-hero.jpg";
 import Button from "../components/Button";
 import Features from "../components/Features";
-import { cn } from "../lib/utils";
+import { useMediaQuery } from "@uidotdev/usehooks";
 import { FC } from "react";
 
 interface HomeProps {
@@ -9,13 +9,12 @@ interface HomeProps {
 }
 
 const Hero: FC<HomeProps> = ({ setLoading }) => {
+  const isSmallDevice = useMediaQuery("only screen and (max-width : 768px)");
   return (
     <>
       <section
         id="home"
-        className={cn(
-          "relative min-h-[600px] from-slate-50 from-30% to-transparent pt-16 md:mb-48 md:bg-gradient-to-r md:to-100% lg:to-60%"
-        )}
+        className="relative min-h-[600px] from-slate-50 from-30% to-transparent pt-16 md:mb-48 md:bg-gradient-to-r md:to-100% lg:to-60%"
       >
         <div className="mx-auto my-10 flex flex-1 flex-col items-center gap-6 px-2 md:max-w-screen-xl md:items-start md:px-8">
           <h1 className="max-w-lg text-center text-4xl font-extrabold capitalize md:max-w-xl md:text-left md:text-5xl md:font-normal">
@@ -28,14 +27,16 @@ const Hero: FC<HomeProps> = ({ setLoading }) => {
           </p>
           <Button> Get started</Button>
         </div>
-        <div className="absolute right-0 top-0 -z-10 h-full md:w-3/4">
-          <img
-            className="hidden h-full w-full object-fill md:block md:object-cover"
-            src={BackgroundImage}
-            alt="illustration"
-            onLoad={() => setLoading(false)}
-          />
-        </div>
+        {!isSmallDevice && (
+          <div className="absolute right-0 top-0 -z-10 h-full md:w-3/4">
+            <img
+              className="hidden h-full w-full object-fill md:block md:object-cover"
+              src={BackgroundImage}
+              alt="illustration"
+              onLoad={() => setLoading(false)}
+            />
+          </div>
+        )}
         <Features />
       </section>
     </>
